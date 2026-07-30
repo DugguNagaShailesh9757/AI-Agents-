7247998f96f21c7dc01e777abc363b8b59e847a2
DUGGU NAGA SHAILESH <nagashailesh477@gmail.com>
2026-07-30 21:46:16 +0530

    Update app.py

--- Travel_Itinerary_Planner_Agent/app.py
+++ Travel_Itinerary_Planner_Agent/app.py
@@ -7,6 +7,8 @@ st.set_page_config(
     page_icon="✈️",
     layout="centered",
     initial_sidebar_state="expanded"
+    with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 )
 
 st.title("✈️ AI Travel Itinerary Planner")
