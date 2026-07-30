b65967eb3e4e1324654a6725456398fdb39b75a5
DUGGU NAGA SHAILESH <nagashailesh477@gmail.com>
2026-07-30 09:28:24 +0530

    Update app.py

--- Smart-Email-Assistant/app.py
+++ Smart-Email-Assistant/app.py
@@ -13,8 +13,7 @@ st.write("Generate professional emails using Google Gemini AI.")
 
 # Temporary API Key
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 recipient = st.text_input("Recipient")
 subject = st.text_input("Subject")
